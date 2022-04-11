754e881476206ce1caf8b8070e1cae44885d4cfc
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-04-12 01:59:01 +0600

    signout component added

--- src/components/Shop/Shipment/Shipment.js
+++ src/components/Shop/Shipment/Shipment.js
@@ -1,11 +1,59 @@
-import React from 'react';
+import React from "react";
 
 const Shipment = () => {
-    return (
-        <div>
-            <h2>This is shipment</h2>
-        </div>
-    );
+  return (
+    <div className="form-container">
+      <div>
+        <h2 className="form-title">Sing Up</h2>
+        <form onSubmit={handleCreateUser}>
+          <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input
+              onBlur={handleEmailBlur}
+              type="email"
+              name="email"
+              id=""
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="password">Password</label>
+            <input
+              onBlur={handlePasswordBlur}
+              type="password"
+              name="password"
+              id=""
+              required
+            />
+          </div>
+          <div className="input-group">
+            <label htmlFor="confirm-password">Confirm Password</label>
+            <input
+              onBlur={handleConfirmPasswordBlur}
+              type="password"
+              name="confirm-password"
+              id=""
+              required
+            />
+          </div>
+          <p style={{ color: "red" }}>{error}</p>
+          <input
+            className="form-submit"
+            type="submit"
+            value="Sign Up"
+            required
+          />
+        </form>
+
+        <p>
+          Already Have an account?
+          <Link className="form-link" to="/login">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default Shipment;
