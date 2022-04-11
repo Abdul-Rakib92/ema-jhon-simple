1a833ec02f305c5e3ad6f97b1293cc72216396fb
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-04-12 03:00:45 +0600

    created & updated shipment component

--- src/components/Shop/Shipment/Shipment.js
+++ src/components/Shop/Shipment/Shipment.js
@@ -1,56 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
+import { useAuthState } from "react-firebase-hooks/auth";
+import auth from "../../../firebase.init";
 
 const Shipment = () => {
+    const [user] = useAuthState(auth);
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('');
+    const [address, setAddress] = useState('');
+    const [phone, setPhone] = useState('');
+    const [error, setError] = useState('');
+    // const navigate = useNavigate();
+
+    const handleNameBlur = event =>{
+        setName(event.target.value);
+    }
+
+    const handleAddressBlur = event =>{
+        setAddress(event.target.value);
+    }
+
+    const handlePhoneBlur = event =>{
+        setPhone(event.target.value);
+    }
+
+    const handleCreateUser = event =>{
+        event.preventDefault();
+        const shipping = {name, email, address, phone};
+        console.log(shipping);
+    }
+
+
   return (
     <div className="form-container">
       <div>
-        <h2 className="form-title">Sing Up</h2>
+        <h2 className="form-title">Your Shipping Info</h2>
         <form onSubmit={handleCreateUser}>
           <div className="input-group">
-            <label htmlFor="email">Email</label>
+            <label htmlFor="name">Your Name</label>
             <input
-              onBlur={handleEmailBlur}
-              type="email"
-              name="email"
-              id=""
-              required
-            />
+              onBlur={handleNameBlur}
+              type="text" name="name" id="" required/>
           </div>
+
           <div className="input-group">
-            <label htmlFor="password">Password</label>
+            <label htmlFor="email">Your Email</label>
             <input
-              onBlur={handlePasswordBlur}
-              type="password"
-              name="password"
-              id=""
-              required
-            />
+              value={user?.email} readOnly type="text" name="email" id="" required/>
           </div>
+
           <div className="input-group">
-            <label htmlFor="confirm-password">Confirm Password</label>
+            <label htmlFor="address">Address</label>
             <input
-              onBlur={handleConfirmPasswordBlur}
-              type="password"
-              name="confirm-password"
-              id=""
-              required
-            />
+              onBlur={handleAddressBlur}
+              type="text" name="address" id="" required/>
           </div>
+
+          <div className="input-group">
+            <label htmlFor="phone">Phone Number</label>
+            <input
+              onBlur={handlePhoneBlur}
+              type="text" name="phone" id="" required/>
+          </div>
+          
+          
           <p style={{ color: "red" }}>{error}</p>
           <input
             className="form-submit"
             type="submit"
-            value="Sign Up"
+            value="Add Shipping"
             required
           />
         </form>
 
-        <p>
-          Already Have an account?
-          <Link className="form-link" to="/login">
-            Login
-          </Link>
-        </p>
       </div>
     </div>
   );
